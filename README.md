5712acf2f73c7060ac306145f0fab50c0961b918
Jonathan Canales <58672259+modern-sapien@users.noreply.github.com>
2023-04-24 17:58:29 +0200

    Updated readme, main.tf & added critical-user-flow-folder for new functionality of advanced example (#4)
    * udpated readme, added critical flows, added dashboard, added maintenance window
    
    * updated capitalization.
    
    * addressed comments in recent PR. udpated main.tf to include PRODUCTS_NUMBER and readme for better use of language.
    
    * updated variable handling for custom URL, so it won't fail due to users applying without adding a url.
    
    * updated main.tf file to have custom_url at top of file & updated readme to also reference custom_url.
    
    * updated custom_url to checkly_dashboard_url and
    updated main.tf file & readme to be more explicit about it's purpose.

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
-# Terraform Basic Sample
+# Terraform Demo Advanced
 
 A sample Terraform setup for Checkly. When applied, will provision:
-- 3 browser checks
+- 7 browser checks
 - 6 (placeholder) API checks
 - 3 groups
 - 2 alert channels
+- 1 maintenance window
+- 1 dashboard
 
-Scripts for browser checks are found in `/scripts`. With the current configuration, a browser check will be created for each script in `/scripts`.
+Scripts are found in `/critical-user-flows`. With the current configuration, a browser check will be created for each spec in `/critical-user-flows`.
 
 ## Usage
 
 1. [Create an account](https://app.checklyhq.com/signup) on Checkly.
 2. Run `terraform init`.
 3. Run `terraform apply`.
-4. When prompted, enter your [Checkly API key](https://app.checklyhq.com/settings/user/api-keys) and [account ID](https://app.checklyhq.com/settings/account/general). You can also export these values to environment variables, respectively `TF_VAR_checkly_api_key` and `TF_VAR_checkly_account_id`, to skip this step.
+4. When prompted, enter your [Checkly API key](https://app.checklyhq.com/settings/user/api-keys)[account ID](https://app.checklyhq.com/settings/account/general) and a checkly_dashboard_url for your [Checkly Public dashboard](https://www.checklyhq.com/docs/terraform-provider/dashboards/).  
+5. The checkly_dashboard_url aids in our creation of a custom dashboard that will display the health of our checks. 
+
+You can export these values to environment variables, respectively `TF_VAR_checkly_api_key`, `TF_VAR_checkly_account_id` and `TF_VAR_checkly_dashboard_url` to skip this step.
 
 ## Resources
 
--- main.tf
+++ main.tf
@@ -1,69 +1,20 @@
 variable "checkly_api_key" {}
 variable "checkly_account_id" {}
+variable "checkly_dashboard_url" {}
 
 terraform {
   required_providers {
     checkly = {
       source = "checkly/checkly"
-      version = "1.4.3"
+      # use fixed version for production setups
+      version = "~> 1.0"
     }
   }
 }
 
 provider "checkly" {
-  api_key = var.checkly_api_key
+  api_key    = var.checkly_api_key
   account_id = var.checkly_account_id
-}
-
-resource "checkly_check_group" "key-shop-flows" {
-  name      = "Key Shop Flows"
-  activated = true
-  muted     = false
-
-  locations = [
-    "eu-west-1",
-    "eu-central-1"
-  ]
-
-  alert_channel_subscription {
-    channel_id = checkly_alert_channel.slack_ac.id
-    activated  = true
-  }
-
-  concurrency = 2
-  environment_variables = {
-    USER_EMAIL = "user@email.com",
-    USER_PASSWORD = "supersecure1",
-    PRODUCTS_NUMBER = 3
-  }
-  double_check              = true
-  use_global_alert_settings = false
-}
-
-resource "checkly_check" "browser-check" {
-  for_each = fileset("${path.module}/scripts", "*")
-
-  name                      = each.key
-  type                      = "BROWSER"
-  activated                 = true
-  should_fail               = false
-  frequency                 = 1
-  double_check              = true
-  ssl_check                 = false
-  use_global_alert_settings = true
-  locations = [
-    "us-west-1",
-    "eu-central-1"
-  ]
-
-  script = file("${path.module}/scripts/${each.key}")
-  group_id = checkly_check_group.key-shop-flows.id
-
-  environment_variables = {
-    "key" = "value"
-  }
-
-
 }
 
 resource "checkly_check" "get-users" {
@@ -157,7 +108,7 @@ resource "checkly_check_group" "users-api" {
     activated  = true
   }
 
-  concurrency = 2
+  concurrency               = 2
   double_check              = true
   use_global_alert_settings = false
 }
@@ -315,7 +266,7 @@ resource "checkly_check_group" "orders-api" {
     "eu-west-1",
     "eu-central-1"
   ]
-  concurrency = 2
+  concurrency               = 2
   double_check              = true
   use_global_alert_settings = false
 
@@ -341,6 +292,80 @@ resource "checkly_alert_channel" "pagerduty_ac" {
 resource "checkly_alert_channel" "slack_ac" {
   slack {
     channel = "#checkly-notifications"
-    url = "https://slack.com/webhook"
+    url     = "https://hooks.slack.com/services/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g"
   }
+}
+
+
+resource "checkly_check_group" "test_group1" {
+  name      = "Critical User Flows"
+  activated = true
+  muted     = false
+  tags = [
+    "terraform", "critical-user-flows"
+  ]
+  concurrency = 10
+  locations = [
+    "us-west-1",
+  ]
+}
+
+resource "checkly_check" "browser-check" {
+  for_each = fileset("${path.module}/critical-user-flows", "*") // Iterates through the files in the scripts folder in your project's directory
+
+  name         = each.key // Sets the check name to match the file's 
+  group_id     = checkly_check_group.test_group1.id
+  type         = "BROWSER"
+  activated    = true
+  frequency    = 60
+  double_check = true
+  ssl_check    = false
+  locations = [
+    "us-west-1",
+    "us-east-1"
+  ]
+  use_global_alert_settings = false
+
+  alert_channel_subscription {
+    channel_id = checkly_alert_channel.slack_ac.id
+    activated  = true
+  }
+  environment_variables = { // This sets check-level environment variables
+    TEST_USER_ID    = "7d8d8288-43a6-44b0-ba08-4c5736c1b6ed",
+    USER_EMAIL      = "user@email.com",
+    USER_PASSWORD   = "supersecure1",
+    FILE_PATH       = "tests/fixtures/file.jpg",
+    TEST_FILE_PATH  = "tests/fixtures/invoice-20-07-02.pdf",
+    ENVIRONMENT_URL = "https://checklyhq.com"
+    PRODUCTS_NUMBER = "3"
+  }
+
+  script = file("${path.module}/critical-user-flows/${each.key}") // Assigns the script contained in each file to each new created check resource
+}
+
+resource "checkly_maintenance_windows" "maintenance-1" {
+  name            = "Maintenance Windows"
+  starts_at       = "2023-04-24T00:00:00.000Z"
+  ends_at         = "2023-04-25T00:00:00.000Z"
+  repeat_unit     = "MONTH"
+  repeat_ends_at  = "2024-08-24T00:00:00.000Z"
+  repeat_interval = 1
+  tags = [
+    "auto", "terraform"
+  ]
+}
+
+resource "checkly_dashboard" "dashboard-main" {
+  custom_url      = var.checkly_dashboard_url                           // A unique subdomain name under "checklyhq.com"
+  custom_domain   = "status.${var.checkly_dashboard_url}.com"               // A custom user domain
+  logo            = "https://www.danube-web.shop/logo.png" // URL pointing to an image/logo for the page
+  header          = "Public dashboard"                     // What text to display at the top of your dashboard
+  refresh_rate    = 60                                     // How often to refresh the dashboard in seconds
+  paginate        = false                                  // Determines if pagination is on or off
+  pagination_rate = 30                                     // How often to trigger pagination in seconds
+  hide_tags       = false                                  // Whether to show or hide the tags on the dashboard
+  width           = "FULL"                                 // Determines whether to use the full screen or focus in the center
+  tags = [                                                 // One or more tags that filter which checks to display on the dashboard
+    "auto", "terraform"
+  ]
 }
